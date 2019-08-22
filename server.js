a499eab101e1fcbc45e219031ca06d4b575d34f2
cwern40 <chris.wernli@hotmail.com>
2019-08-21 18:12:22 -0600

    I created the get, get by ID, and post operations

--- server.js
+++ server.js
@@ -6,4 +6,47 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.json(accounts)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "error retrieving accounts"
+            })
+        })
+})
+
+server.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    db('accounts').where({ id })
+        .then(account => {
+            res.json(account)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: 'error retrieving account'
+            })
+        })
+})
+
+server.post('/', (req, res) => {
+    const accountData = req.body
+
+    db('accounts').insert(accountData)
+        .then(account => {
+            res.status(201).json(account[0])
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "Unable to add account"
+            })
+        })
+})
+
 module.exports = server;
